fdbf669001ac796175c9b7eff1783d00e992822f
tekkub ʕ ´ᴥ` ʔ <tekkub@users.noreply.github.com>
2017-07-28 22:18:52 -0600

    Pull some layout numbers out into constants

--- house.lua
+++ house.lua
@@ -7,16 +7,18 @@ local Class, House = class()
 local NUM_ROWS = 6
 local NUM_COLS = 4
 local ROOM_SIZE = 80
+local HOUSE_MARGIN = 28
+local ROOM_MARGIN = 14
 
 local rooms = {}
 
 
 function House:Initialize()
   Room:SetSize(ROOM_SIZE)
-  local y_offset = 20
+  local y_offset = HOUSE_MARGIN
 
   for row=1,NUM_ROWS do
-    local x_offset = 20
+    local x_offset = HOUSE_MARGIN
     rooms[row] = {}
 
     for col=1,NUM_COLS do
@@ -24,10 +26,10 @@ function House:Initialize()
       room.row, room.col = row, col
       rooms[row][col] = room
 
-      x_offset = x_offset + ROOM_SIZE + 10
+      x_offset = x_offset + ROOM_SIZE + ROOM_MARGIN
     end
 
-    y_offset = y_offset + ROOM_SIZE + 10
+    y_offset = y_offset + ROOM_SIZE + ROOM_MARGIN
   end
 end
 
